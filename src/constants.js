0f2f88c4d02a005289fb3f7d314d273e6b0e8593
Nissi126 <46329278+Nissi126@users.noreply.github.com>
2020-11-18 23:04:22 -0600

    Fetch random question (#44)
    * add random question functionality to QuizMaster view. Set up endpoint to fetch filtered random question. Set up dropdown to select question type.
    
    * Set up multiselect for chapters. Defalut all selected. Could use better var names?
    
    * Make constants file and clean up some code.
    
    * Fixed Looping
    
    * Adjusted spelling.
    
    * Start quiz with "Random Question" when question number is at zero.
    
    * Group Real Quiz and Practice Quiz buttons and information.
    
    * Updated with Nadia/my collab code.
    
    Co-authored-by: ljsnell <ljsnell7@gmail.com>

---
+++ src/constants.js
@@ -0,0 +1,170 @@
+const chapters = [{
+    value: 1,
+    label: 1,
+    color: '#5243AA',
+    isFixed: false,
+    isDisabled: false  
+},{
+    value: 2,
+    label: 2,
+    color: '#5243AA',
+    isFixed: false,
+    isDisabled: false  
+},{
+    value: 3,
+    label: 3,
+    color: '#5243AA',
+    isFixed: false,
+    isDisabled: false  
+},{
+    value: 4,
+    label: 4,
+    color: '#5243AA',
+    isFixed: false,
+    isDisabled: false  
+},{
+    value: 5,
+    label: 5,
+    color: '#5243AA',
+    isFixed: false,
+    isDisabled: false  
+},{
+    value: 6,
+    label: 6,
+    color: '#5243AA',
+    isFixed: false,
+    isDisabled: false  
+},{
+    value: 7,
+    label: 7,
+    color: '#5243AA',
+    isFixed: false,
+    isDisabled: false  
+},{
+    value: 8,
+    label: 8,
+    color: '#5243AA',
+    isFixed: false,
+    isDisabled: false  
+},{
+    value: 9,
+    label: 9,
+    color: '#5243AA',
+    isFixed: false,
+    isDisabled: false  
+},{
+    value: 10,
+    label: 10,
+    color: '#5243AA',
+    isFixed: false,
+    isDisabled: false  
+},{
+    value: 11,
+    label: 11,
+    color: '#5243AA',
+    isFixed: false,
+    isDisabled: false  
+},{
+    value: 12,
+    label: 12,
+    color: '#5243AA',
+    isFixed: false,
+    isDisabled: false  
+},{
+    value: 13,
+    label: 13,
+    color: '#5243AA',
+    isFixed: false,
+    isDisabled: false  
+},{
+    value: 14,
+    label: 14,
+    color: '#5243AA',
+    isFixed: false,
+    isDisabled: false  
+},{
+    value: 15,
+    label: 15,
+    color: '#5243AA',
+    isFixed: false,
+    isDisabled: false  
+},{
+    value: 16,
+    label: 16,
+    color: '#5243AA',
+    isFixed: false,
+    isDisabled: false  
+},{
+    value: 17,
+    label: 17,
+    color: '#5243AA',
+    isFixed: false,
+    isDisabled: false  
+},{
+    value: 18,
+    label: 18,
+    color: '#5243AA',
+    isFixed: false,
+    isDisabled: false  
+},{
+    value: 19,
+    label: 19,
+    color: '#5243AA',
+    isFixed: false,
+    isDisabled: false  
+},{
+    value: 20,
+    label: 20,
+    color: '#5243AA',
+    isFixed: false,
+    isDisabled: false  
+},{
+    value: 21,
+    label: 21,
+    color: '#5243AA',
+    isFixed: false,
+    isDisabled: false  
+},{
+    value: 22,
+    label: 22,
+    color: '#5243AA',
+    isFixed: false,
+    isDisabled: false  
+},{
+    value: 23,
+    label: 23,
+    color: '#5243AA',
+    isFixed: false,
+    isDisabled: false  
+},{
+    value: 24,
+    label: 24,
+    color: '#5243AA',
+    isFixed: false,
+    isDisabled: false  
+},{
+    value: 25,
+    label: 25,
+    color: '#5243AA',
+    isFixed: false,
+    isDisabled: false  
+},{
+    value: 26,
+    label: 26,
+    color: '#5243AA',
+    isFixed: false,
+    isDisabled: false  
+},{
+    value: 27,
+    label: 27,
+    color: '#5243AA',
+    isFixed: false,
+    isDisabled: false  
+},{
+    value: 28,
+    label: 28,
+    color: '#5243AA',
+    isFixed: false,
+    isDisabled: false  
+}];
+export { chapters as default }
