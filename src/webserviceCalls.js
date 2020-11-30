5f633cc35e6be4bc576df2d6373c9841c89e40df
LAPTOP-CPQLSHDE\littlemag126 <callerofhope7@gmail.com>
2020-11-30 16:50:49 -0600

    all logic and ui exepct the extra * information to inform that Bonus is not part of this.

--- src/webserviceCalls.js
+++ src/webserviceCalls.js
@@ -3,6 +3,11 @@ const fetchQuestion = (questionID) => {
   // return fetch('http://localhost:5000/?QID='+questionID)
 }
 
+const fetchQuestionType =  (questionID) => {    
+  return fetch('https://bible-questions-api.herokuapp.com/question-type?QID='+questionID)            
+  // return fetch('http://localhost:5000/question-type?QID='+questionID)
+}
+
 const fetchRandomQuestion = (qtype, books, chapters) => {    
   return fetch('https://bible-questions-api.herokuapp.com/filtered?qtype='+qtype+'&books='+books+'&chapters='+chapters)
   // return fetch('http://localhost:5000/filtered?qtype='+qtype+'&books='+books+'&chapters='+chapters)
