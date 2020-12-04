c7cb32b73620963a7c3a92daca1179a39162852b
ljsnell <ljsnell7@gmail.com>
2020-12-04 00:06:00 -0600

    Updated formatting.

--- src/login/Login.js
+++ src/login/Login.js
@@ -20,19 +20,12 @@ export default function Login() {
         <Form>
             <FormGroup>
                 <Label for="roomNumber">Room #</Label>
-                <Input type="roomNum" name="roomNum" id="roomNumID" placeholder="room"/>
+                <Input type="roomNum" name="roomNum" id="roomNumID" placeholder="room" />
             </FormGroup>
             <FormGroup>
                 <Label for="username">Room #</Label>
-                <Input type="userName" name="userName" id="userNameID" placeholder="E.G. 1-Jeff-Gnarwhals3.0"/>
+                <Input type="userName" name="userName" id="userNameID" placeholder="E.G. 1-Jeff-Gnarwhals3.0" />
             </FormGroup>
             <div className="footerButton" ><Button onClick={() => this.handleSubmit()}><h2>Login</h2></Button>{' '}</div>
-        </Form>
-        // <div className="Login">
-        //     <h2>Hi</h2>
-        //     <div className="footerButton" ><Button onClick={() => this.handleSubmit()}><h2>Login</h2></Button>{' '}</div>
-        // </div>
-        // var user_room = prompt("Please enter your room #", "room");
-        // var entered_username = prompt("Please enter your user name. E.G. 1-Jeff-Gnarwhals3.0", "Username");
-    );
+        </Form>);
 }
