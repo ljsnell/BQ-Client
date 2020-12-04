944c36b257fb7222b10d21909422af4d79b76275
ljsnell <ljsnell7@gmail.com>
2020-12-03 23:47:04 -0600

    Updated with reactstrap imports.

--- src/login/Login.js
+++ src/login/Login.js
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { Button, Form } from 'reactstrap';
+import { Button, Form, Label, Input, FormGroup } from 'reactstrap';
 import 'medium-editor/dist/css/medium-editor.css';
 import 'medium-editor/dist/css/themes/default.css';
 import '../App.css';
@@ -16,11 +16,22 @@ export default function Login() {
         event.preventDefault();
     }
 
-    return (
-        <div className="Login">
-            <h2>Hi</h2>
+    return ( // https://reactstrap.github.io/components/form/
+        <Form>
+            <FormGroup>
+                <Label for="roomNumber">Room #</Label>
+                <Input type="roomNum" name="roomNum" id="roomNumID" placeholder="room"/>
+            </FormGroup>
+            <FormGroup>
+                <Label for="username">Room #</Label>
+                <Input type="userName" name="userName" id="userNameID" placeholder="E.G. 1-Jeff-Gnarwhals3.0"/>
+            </FormGroup>
             <div className="footerButton" ><Button onClick={() => this.handleSubmit()}><h2>Login</h2></Button>{' '}</div>
-        </div>
+        </Form>
+        // <div className="Login">
+        //     <h2>Hi</h2>
+        //     <div className="footerButton" ><Button onClick={() => this.handleSubmit()}><h2>Login</h2></Button>{' '}</div>
+        // </div>
         // var user_room = prompt("Please enter your room #", "room");
         // var entered_username = prompt("Please enter your user name. E.G. 1-Jeff-Gnarwhals3.0", "Username");
     );
