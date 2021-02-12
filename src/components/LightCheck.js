f51e63e715f2d1d7d54672463a9f7cfc5638fe25
ljsnell <ljsnell7@gmail.com>
2021-02-12 00:02:45 -0600

    Options for light check.

--- src/components/LightCheck.js
+++ src/components/LightCheck.js
@@ -14,10 +14,12 @@ const LightCheck = () => {
         <>
             <div>
                 <>Select Chair to Test: </>
-                <Select>
+                <Select native value='1'>
                     <option key='1' value='1'>1</option>
                     <option key='2' value='2'>2</option>
                     <option key='3' value='3'>3</option>
+                    <option key='4' value='4'>4</option>
+                    <option key='5' value='5'>5</option>
                 </Select >
             </div>
         </>
