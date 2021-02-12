6ed23a8a509f24083fd4b866d8edf22076a674f0
ljsnell <ljsnell7@gmail.com>
2021-02-11 23:53:52 -0600

    Need to loop through options for Select next.

--- src/components/LightCheck.js
+++ src/components/LightCheck.js
@@ -1,34 +1,28 @@
-import { Button, Dialog, DialogActions, DialogContent, DialogContentText, DialogTitle, List, ListItem, ListItemIcon, ListItemText } from '@material-ui/core';
+import { Select } from '@material-ui/core';
 import React from 'react';
+
+const options = [
+    '1',
+    '2',
+    '3',
+    '4',
+    '5'
+];
 
 const LightCheck = () => {
     return (
-        <div>List of Chairs to check here</div>
+        <>
+            <div>
+                <>Select Chair to Test: </>
+                <Select>
+                    <option key='1' value='1'>1</option>
+                    <option key='2' value='2'>2</option>
+                    <option key='3' value='3'>3</option>
+                </Select >
+            </div>
+        </>
     )
+    // <div>Choose a chair to send a jump to: </div>
 }
 
 export default LightCheck
-
-// const QUIZZER_STYLE = {
-//     root: {
-//         width: '80%',
-//         maxWidth: '80vw',
-//         maxHeight: '80vh',
-//         position: 'fixed',
-//         top: '50%',
-//         left: '10%',
-//         transform: 'translate(0, -50%)',
-//         overflowY: 'auto'
-//     },
-//     bullet: {
-//         display: 'inline-block',
-//         margin: '0 2px',
-//         transform: 'scale(0.8)',
-//     },
-//     title: {
-//         fontSize: 14,
-//     },
-//     pos: {
-//         marginBottom: 12,
-//     },
-// }
