8f98313c222e8452a8bedb9c592eee47a8ada2f4
ljsnell <ljsnell7@gmail.com>
2021-02-13 00:52:39 -0600

    Logs the selected value.

--- src/components/LightCheck.js
+++ src/components/LightCheck.js
@@ -1,5 +1,6 @@
 import { Select } from '@material-ui/core';
 import React from 'react';
+import { Component } from 'react';
 
 const options = [
     '1',
@@ -9,21 +10,27 @@ const options = [
     '5'
 ];
 
-const LightCheck = () => {
-    return (
-        <>
-            <div>
-                <>Select Chair to Test: </>
-                <Select native value='1'>
-                    <option key='1' value='1'>1</option>
-                    <option key='2' value='2'>2</option>
-                    <option key='3' value='3'>3</option>
-                    <option key='4' value='4'>4</option>
-                    <option key='5' value='5'>5</option>
-                </Select >
-            </div>
-        </>
-    )
+class LightCheck extends Component {
+    handleChanges = (event) => {
+        console.log(event.target.value)
+    }
+
+    render() {
+        return (
+            <>
+                <div>
+                    <>Select Chair to Test: </>
+                    <Select native={true} onChange={this.handleChanges} native value='1'>
+                        <option key='1' value='1'>1</option>
+                        <option key='2' value='2'>2</option>
+                        <option key='3' value='3'>3</option>
+                        <option key='4' value='4'>4</option>
+                        <option key='5' value='5'>5</option>
+                    </Select >
+                </div>
+            </>
+        )
+    }
 }
 
 export default LightCheck
